1b47604c38fd8af496d30ff386957934fd3e4250
BIINBELLLO <abdulmateenbinbello@gmail.com>
2019-02-07 15:34:03 +0100

    Login Working

---
+++ includes/verify_user_details.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+	include('db_connection.php');
+	include('user_class.php');
+	if (isset($_POST['login'])) {
+		$Email = $_POST['email'];
+		$Password = $_POST['pswd'];
+
+		$logged_in_user = new User;
+		$User_Data = $logged_in_user->loginUser($Email, $Password);
+
+		if ($User_Data == false) {
+			$_SESSION['Error'] = "Wrong Credentials, Access Denied!";
+			header("Location: ../login.php");
+		}else{
+			$_SESSION['user'] = $User_Data['Email'];
+			header("Location: ../dashboard.php");
+		}
+
+	}
+?>
